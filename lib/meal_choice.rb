b33d9808f761ad63532ab2c8b67884ff72fc90a0
Patrick Harrison <jahman@bellatlantic.net>
2019-06-28 23:46:47 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,5 +1,5 @@
 def meal_choice (protein, veg1,veg2)
      puts "what a nutrious meal."
-     puts "a plate of #{protein}, with #{veg1}, + #{veg2} so that you can also see what you ordered."
+     puts "a plate of #{protein}, with #{veg1} #{veg2} so that you can also see what you ordered."
      end
 meal_choice ("meat"),("broccoli"),("macaroni")
